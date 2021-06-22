64979118b671b8468cbe4b3f52a3d94d1bfb0f98
Kadir Göçebe <kadirgocebe@Kadir-MacBook-Air.local>
2021-06-23 00:03:46 +0300

    ilk terminal kulanımı ile ekleme

---
+++ PatikaOdev0.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        Scanner girdi = new Scanner(System.in);
+        int a;
+
+            System.out.println(" a sayısı : "+a);
+            mat = girdi.nextInt();
+
+}
+
+}
