7ccf4f3b1a2bde95faccfd75ebc1761dc9b8e16a
Kadir Gocebe <kadir.gocebe@gmail.com>
2021-07-14 12:15:23 +0300

    yeniOdev

---
+++ kdv.java
@@ -0,0 +1,27 @@
+package hellojava;
+import java.util.Scanner;
+
+public class kdv {
+    public static void main(String[] args) {
+        double para; double oran1 = 0.18; double oran2 = 0.08; double kdvTutari; double kdvliPara;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Para Miktarını Giriniz : ");
+        para = input.nextDouble();
+    
+     if(para<=1000){
+         kdvTutari = para * oran1 ; 
+         kdvliPara = kdvTutari + para; 
+        System.out.println("Girilen Tutar : " + para);
+        System.out.println("Hesaplanan KDV Tutarı : "+ kdvTutari);
+        System.out.println("KDV dahil Tutar : " + kdvliPara);
+        }
+        else {
+            kdvTutari = para * oran2 ; 
+            kdvliPara = kdvTutari + para; 
+           System.out.println("Girilen Tutar : " + para);
+           System.out.println("Hesaplanan KDV Tutarı : "+ kdvTutari);
+           System.out.println("KDV dahil Tutar : " + kdvliPara);
+
+        }
+    }
+}
