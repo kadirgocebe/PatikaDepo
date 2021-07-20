6fdcd7a0f731492b69eacc0dcbc31b6d4a23e6c8
Kadir Gocebe <kadir.gocebe@gmail.com>
2021-07-20 15:40:13 +0300

    asalProg

---
+++ asal.java
@@ -0,0 +1,28 @@
+
+public class asal {
+    public static void main(String[] args) {
+        int sayac = 0;
+
+        for(int i=2;i<=100;i++)
+        {
+            int kontrol = 0;
+            for (int j = 2; j < i; j++)
+            {
+               if (i % j == 0)
+                {
+                    kontrol = 1;
+                    break;
+                } 
+            }
+                
+            if(kontrol==0)
+            {
+                System.out.print(i+" ");
+                sayac++; 
+            }
+        }
+        System.out.println(" ");
+        System.out.print("Toplamda " +sayac +" sayı asaldır");
+    }  
+    }
+
