555364b90cbcb361e68cbc9a5467931a0f2e8024
kadirgocebe <kadir.gocebe@gmail.com>
2021-08-13 15:56:53 +0300

    tekrarEdenSayilarOdevi

---
+++ tekrarEdenSayilar.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+public class tekrarEdenSayilar {
+
+    static boolean isFind(int[] arr, int value) {
+        for (int i : arr) {
+            if (i == value) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+       
+        int counter=0;
+        int[] list = {3, 9, 3, 3, 9, 9, 10, 21, 1, 33, 9, 1};
+        int[] duplicate = new int[list.length];
+        int startIndex = 0;
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length; j++) {
+                if ((i != j) && (list[i] == list[j])) {
+                    if (!isFind(duplicate, list[i])) {
+                        duplicate[startIndex++] = list[i];
+                        counter ++;
+                    }
+                    
+                    break;
+                    
+                }
+            }
+        }
+        
+        
+        for (int value : duplicate){
+            if (value != 0){
+                System.out.println("Tekrar Eden sayı = "+value);
+                
+                
+            }
+        }
+
+        
+    }
+}
+    
+
